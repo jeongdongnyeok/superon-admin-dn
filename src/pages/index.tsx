40b455204ea4f358aee5b9b8340b079af487ca6e
ethan <ethan@ethans-MacBook-Pro.local>
2025-05-02 16:11:26 +0900

    Fix: index file import withAuth resolved

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+// src/pages/index.tsx
+import withAuth from '@/lib/withAuth'
+
 export default withAuth(function Dashboard() {
-    return <div>Welcome to the dashboard!</div>
-  })
+  return <div>Welcome to the dashboard!</div>
+})
